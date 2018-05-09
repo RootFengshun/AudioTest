ed24f20bfad202b9f71c545a175e6f2ff878715e
fengshun <fengshun@bytedance.com>
2018-05-10 00:15:08 +0800

    建立工程，初始化项目

---
+++ app/src/main/java/com/fengshun/audiotest/UnitView.java
@@ -0,0 +1,14 @@
+package com.fengshun.audiotest;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * Created by fengshun on 2018/5/9.
+ */
+public class UnitView extends LinearLayout {
+    public UnitView(Context context) {
+        super(context);
+    }
+
+}
