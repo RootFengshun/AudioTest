6161ac3c1329b246be25e69ba8baa7d26fe7fefc
fengshun <fengshun@163.com>
2018-05-14 21:41:20 +0800

    增加wav文件的读写；

--- app/src/main/java/com/fengshun/audiotest/MainActivity.java
+++ app/src/main/java/com/fengshun/audiotest/MainActivity.java
@@ -2,18 +2,53 @@ package com.fengshun.audiotest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.fengshun.audiotest.tester.AudioCaptureTester;
+import com.fengshun.audiotest.tester.AudioPlayerTester;
+import com.fengshun.audiotest.tester.Tester;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
+    private Tester mTester;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UnitView pcmView =  (UnitView)findViewById(R.id.pcm_test);
+        pcmView.setClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("atom", "hhh");
+                switch (v.getId()) {
+                    case R.id.start_record:
+                        mTester = new AudioCaptureTester();
+                        mTester.startTesting();
+                        break;
+                    case R.id.stop_record:
+                        mTester.stopTesting();
+                        break;
+                    case R.id.start_play:
+                        mTester = new AudioPlayerTester();
+                        mTester.startTesting();
+                        break;
+                    case R.id.stop_play:
+                        mTester.stopTesting();
+                        break;
+                }
+            }
+        });
+
+
+        UnitView realtimeView = (UnitView) findViewById(R.id.realtime_test);
+
 
 
     }
@@ -21,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
+     * public native String stringFromJNI();
      */
-    public native String stringFromJNI();
+
 }
--- app/src/main/java/com/fengshun/audiotest/UnitView.java
+++ app/src/main/java/com/fengshun/audiotest/UnitView.java
@@ -1,14 +1,51 @@
 package com.fengshun.audiotest;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 /**
  * Created by fengshun on 2018/5/9.
  */
-public class UnitView extends LinearLayout {
+public class UnitView extends LinearLayout{
+    Button mStartRecord;
+    Button mStopRecord;
+    Button mStartPlay;
+    Button mStopPlay;
+    OnClickListener mClickListener;
+
     public UnitView(Context context) {
         super(context);
+        LayoutInflater.from(context).inflate(R.layout.unit_view_layout, this);
+        initViews();
+    }
+    public UnitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.unit_view_layout, this);
+        initViews();
+    }
+    public UnitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context,attrs, defStyle);
+        LayoutInflater.from(context).inflate(R.layout.unit_view_layout, this);
+        initViews();
+    }
+    private void initViews() {
+        mStartRecord = (Button) findViewById(R.id.start_record);
+        mStopRecord = (Button) findViewById(R.id.stop_record);
+        mStartPlay = (Button) findViewById(R.id.start_play);
+        mStopPlay = (Button) findViewById(R.id.stop_play);
     }
 
+
+    public void setClickListener(OnClickListener clickListener) {
+        mClickListener = clickListener;
+        mStartPlay.setOnClickListener(mClickListener);
+        mStopRecord.setOnClickListener(mClickListener);
+        mStartRecord.setOnClickListener(mClickListener);
+        mStopPlay.setOnClickListener(mClickListener);
+    }
 }
